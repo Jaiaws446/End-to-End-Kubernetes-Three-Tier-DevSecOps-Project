e430a6683bf4b5599ca92d8c7fc5a76635b9405f
jaiaws446 <12192jaipal@gmail.com>
2024-08-30 08:26:48 +0530

    backendconfig

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,15 +1,15 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "jaiaws446-state-dev"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "jaiaws446-locking-dev"
     encrypt        = true
   }
   required_version = ">=0.13.0"
   required_providers {
     aws = {
-      version = ">= 2.7.0"
+      version = "5.31.0"
       source  = "hashicorp/aws"
     }
   }
